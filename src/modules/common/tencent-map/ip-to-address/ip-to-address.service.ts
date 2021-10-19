af20bdf2eab0aab7479e53aa716015971fb00898
Vito Makhatadze <vitomaxatadze@gmail.com>
2021-10-19 16:09:50 +0400

    Translation ip address default value on development.

--- src/modules/common/tencent-map/ip-to-address/ip-to-address.service.ts
+++ src/modules/common/tencent-map/ip-to-address/ip-to-address.service.ts
@@ -1,22 +1,22 @@
-import { Injectable } from "@nestjs/common";
-import * as superagent from "superagent";
+import { Injectable } from '@nestjs/common';
+import * as superagent from 'superagent';
 
 @Injectable()
 export class IpToAddressService {
   public async IpToAddress(ip: string): Promise<string | null> {
-    if (ip === "::1") {
-      return "开发环境运行";
+    if (ip === '::1') {
+      return 'Development environment operation';
     }
     try {
       const responseText = await superagent
-        .get("https://apis.map.qq.com/ws/location/v1/ip")
+        .get('https://apis.map.qq.com/ws/location/v1/ip')
         .query({ ip })
-        .query({ output: "json" })
+        .query({ output: 'json' })
         .query({ key: process.env.TENCENT_MAP_KEY });
       const {
         status,
         // eslint-disable-next-line
-        result: { ad_info }
+        result: { ad_info },
       } = JSON.parse(responseText.text) || {};
       if (status == 0) {
         // eslint-disable-next-line
@@ -26,8 +26,8 @@ export class IpToAddressService {
         return null;
       }
     } catch (e) {
-      console.log(e, "地址解析错误");
-      throw new Error(`${ip}地址解析错误`);
+      console.log(e, 'Geocoding error');
+      throw new Error(`${ip}Geocoding error`);
     }
   }
 }
